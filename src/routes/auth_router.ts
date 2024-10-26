f283e1b0bf623339d1842b14f2dc8dea35a9b5fb
Yehia Mohamed <yehiamohamed915@yahoo.com>
2024-10-26 17:14:29 +0300

    adding JWT utils (generate,verfiy) tokens implement login feature (tested)

--- src/routes/auth_router.ts
+++ src/routes/auth_router.ts
@@ -4,5 +4,6 @@ import {AuthController} from "../controllers/auth_controller";
 const authRouter = express.Router();
 
 authRouter.post("/auth/register",AuthController.register); 
+authRouter.post("/auth/login",AuthController.login); 
 
 export default authRouter;
